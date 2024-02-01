02a8fab8c0a58e65e2f88301a847a1a93f2b1d2a
Haris <hariskhan1435@gmail.com>
2024-02-01 16:21:26 +0500

    Update scripts/publish.sh
    Co-authored-by: Uman Shahzad <uman@mslm.io>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,9 +9,7 @@ twine upload dist/*
 echo "Cleaning up..."
 python3 setup.py clean --all
 
-# clean egs
-rm -rf mslm.egg-info
-rm -rf mslm_email_verify.egg-info
-rm -rf mslm_otp.egg-info
+# clean build artifacts
+rm -rf *.egg-info
 
 echo "Done."
